6651dc47815e8151de4ce4004af9f8ba2bc2dd45
vad3l <vandrepoleo@gmail.com>
2024-12-05 23:11:29 +0100

    oui

--- public/js/global/themes.js
+++ public/js/global/themes.js
@@ -1,29 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
 
+
+document.addEventListener('DOMContentLoaded', () => {
     const themes = ['light', 'dark', 'santa'];
     let currentTheme = localStorage.getItem('theme') || themes[0];
 
-    body.classList.add(currentTheme);
-
-    const changeTheme = () => {
-        body.classList.add('theme-transition');
-
-        const nextTheme = themes[(themes.indexOf(currentTheme) + 1) % themes.length];
-
-        body.classList.replace(currentTheme, nextTheme);
-        currentTheme = nextTheme;
-
-        localStorage.setItem('theme', currentTheme);
-
-        window.showPopup(window.getTranslation('front.theme_update')+': '+currentTheme);
-
-        setTimeout(() => {
-            body.classList.remove('theme-transition');
-        }, 1000);
+    const applyTheme = (theme, bodyElement) => {
+        if (bodyElement) {
+            bodyElement.className = '';
+            bodyElement.classList.add('theme-transition');
+            bodyElement.classList.add(theme);
+            
+            setTimeout(() => {
+                bodyElement.classList.remove('theme-transition');
+            }, 1000);
+        }
     };
 
-    body.addEventListener('click', (event) => {
+    const changeTheme = () => {
+        const nextTheme = themes[(themes.indexOf(currentTheme) + 1) % themes.length];
+
+        applyTheme(nextTheme, document.body);
+
+        const iframes = Array.from(document.querySelectorAll('iframe'));
+        iframes.forEach(iframe => {
+            try {
+                const iframeBody = iframe.contentDocument?.body;
+                applyTheme(nextTheme, iframeBody);
+            } catch (error) {
+                console.error('Impossible d\'accéder au contenu de l\'iframe : ', error);
+            }
+        });
+
+        currentTheme = nextTheme;
+        localStorage.setItem('theme', currentTheme);
+        
+    };
+
+    applyTheme(currentTheme, document.body);
+
+    const iframes = Array.from(document.querySelectorAll('iframe'));
+    iframes.forEach(iframe => {
+        iframe.addEventListener('load', () => {
+            try {
+                const iframeBody = iframe.contentDocument?.body;
+                applyTheme(currentTheme, iframeBody);
+            } catch (error) {
+                console.error('Impossible d\'accéder au contenu de l\'iframe : ', error);
+            }
+        });
+    });
+
+    document.body.addEventListener('click', (event) => {
         if (event.target.classList.contains('snowflake')) {
             changeTheme();
         }
