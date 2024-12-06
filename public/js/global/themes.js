07eae2b85bb4921c7d984538b1436c1cfc80ca98
UnExtraterrestres <wfoucon@proton.me>
2024-12-06 05:11:29 +0100

    oui, c'est encore un daltonien qui a géré les couleurs

--- public/js/global/themes.js
+++ public/js/global/themes.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const themes = ['light', 'dark', 'santa', 'clean', 'rain', 'cloudy', 'strom', 'snow'];
+    const themes = ['light', 'dark', 'santa', 'clean', 'rain', 'cloudy', 'storm', 'snow'];
     let currentTheme = localStorage.getItem('theme') || themes[0];
 
     const applyTheme = (theme, bodyElement) => {
